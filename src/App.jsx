7b480e587426c44c65e5bebfc4eee08649972bbc
SethGripon <setharvinpgripon@iskolarngbayan.pup.edu.ph>
2024-08-05 07:35:35 +0800

    added route in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import './index.css'
 
-function App() {
+// Routes
+import Home from './components/Home'
 
+function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-lightOne">
-        Hello world!
-      </h1>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
       <Navbar />
-    </div>
+    </>
   )
 }
 
